59b3b5ad585af0aaca1a1a447243f7f9546b1430
Richard Todd <rwtodd@users.noreply.github.com>
2023-06-16 23:19:16 -0500

    small changes to english::gematria

--- English-Gematria/lib/English/Gematria.pm
+++ English-Gematria/lib/English/Gematria.pm
@@ -2,15 +2,14 @@ package English::Gematria;
 
 use v5.36;
 use Exporter qw/import/;
-use Carp;
+use Carp ();
 our $VERSION = '0.01';
 
 our %EXPORT_TAGS = ( 'all' => [ qw(
-        
+ names_ciphers make_cipher synonyms 
 ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
 our @EXPORT = qw();
 
 my %ciphers = (
@@ -33,18 +32,22 @@ my %ciphers = (
 sub named_ciphers() { keys %ciphers }
 
 package English::Gematria::Cipher {
-  use Carp;
+  use Carp ();
   use List::Util ();
   sub sum($self, $str) {
     List::Util::sum map { $self->{$_} // 0 } split(//, $str) 
   }
 
   sub new($cls,$cipher_hash) {
-    croak 'Cipher definition must be a hash ref!' unless( ref($cipher_hash) eq 'HASH' );
+    Carp::croak 'Cipher definition must be a hash ref!' unless( ref($cipher_hash) eq 'HASH' );
     bless $cipher_hash, $cls;
   }
 
-  sub describe($self, $fh) {
+  sub add_to_lexicon($self, $lexicon, @words) {
+    $lexicon->{$_} = $self->sum($_) for (@words);
+  }
+
+  sub describe($self, $fh=\*STDOUT) {
     my $count = 0;
     for my $k ('A' .. 'Z') {
       my $val = $self->{$k} // 0;
@@ -64,7 +67,7 @@ sub make_cipher($cipher) {
   my %result;
   my @nums;
   $cipher = $ciphers{$cipher} if(ref($cipher) eq '');
-  croak 'Bad argument for make_cipher' unless(ref($cipher) eq 'ARRAY'); 
+  Carp::croak 'Bad argument for make_cipher' unless(ref($cipher) eq 'ARRAY'); 
   my $idx = 0;
   for my $letter ('A' .. 'Z') {
     $result{lc $letter} = $result{$letter} = $$cipher[$idx++]; 
@@ -91,59 +94,52 @@ English::Gematria - Perl extension for blah blah blah
 =head1 SYNOPSIS
 
   use English::Gematria;
-  blah blah blah
+  my $alw = English::Gematria::make_cipher('alw');
+  say $alw->sum('one');  # 46
 
 =head1 DESCRIPTION
 
-Stub documentation for English::Gematria, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module supports a number of common "Enlgish Qabalah" gematria ciphers.
 
-(def alw-cipher
-  "The default english qabalah letter values."
-  (generate-cipher [1 20 13 6 25 18 11 4 23 16 9 2 21 14 7 26 19 12 5 24 17 10 3 22 15 8]))
+=over 4
 
-(def love-x-cipher
-  "an inversion of the ALW cipher -- see New Order of Thelema"
-  (generate-cipher [9 20 13 6 17 2 19 12 23 16 1 18 5 22 15 26 11 4 21 8 25 10 3 14 7 24]))
+=item alw
 
-(def liber-cxv-cipher
-  "from linda falorio 1978 http://englishqabalah.com/"
-  (generate-cipher [1 5 9 12 2 8 10 0 3 6 9 14 6 13 4 7 18 15 16 11 5 8 10 11 6 32]))
+The default english qabalah letter values. Named for A=1, L=2, W=3.
 
-(def leeds-cipher
-  "1-7-1 1-7-1 cipher (see https://grahamhancock.com/leedsm1/)"
-  (generate-cipher (cycle (concat (range 1 7) (range 7 0 -1)))))
+=item love-x
 
-(def simple-cipher
-  "A-Z 1-26"
-  (generate-cipher (range 1 27)))
+An inversion of the ALW cipher -- see New Order of Thelema
 
-(def trigrammaton-cipher
-  "From R. Leo Gillis TQ (trigrammaton qabalah)"
-  (generate-cipher [5 20 2 23 13 12 11 3 0 7 17 1 21 24 10 4 16 14 15 9 25 22 8 6 18 19]))
+=item liber-cxv
 
-(def liber-a-cipher
-  "Liber A vel Follis https://hermetic.com/wisdom/lib-follis"
-  (generate-cipher (range 26)))
-Blah blah blah.
+From linda falorio, 1978 (see http://englishqabalah.com/).
+
+=item leeds
+
+1-7-1 1-7-1 cipher (see https://grahamhancock.com/leedsm1/).
+
+=item simple
+
+A-Z 1-26
+
+=item trigrammaton
+
+From R. Leo Gillis TQ (trigrammaton qabalah).
+
+=item liber-a
+
+Liber A vel Follis (see https://hermetic.com/wisdom/lib-follis).
+
+=back
 
 =head2 EXPORT
 
-None by default.
-
-
+None by default.  Supports C<:all> to get at the functions.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+TODO
 
 =head1 AUTHOR
 
